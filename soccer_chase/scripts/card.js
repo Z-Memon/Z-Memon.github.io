6f6f5d79a378d5ff83bc48a65794191853b86793
Z-Memon <zehra.memon@georgebrown.ca>
2024-04-12 10:01:36 -0400

    corrected file path

--- soccer_chase/scripts/card.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-    const currentPage = window.location.pathname.split('/').pop();
-    const navItems = document.querySelectorAll('.nav-item');
-    navItems.forEach(item => {
-      if (item.href.includes(currentPage)) {
-        item.classList.add('active');
-      }
-    });
-  };
