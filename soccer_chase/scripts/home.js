9ca82bd8706d5943df8cc85c42bc19adaeb34d94
Tarasyuha <111252613+Tarasyuha@users.noreply.github.com>
2024-04-12 23:45:48 -0400

    settings icon on home page

--- soccer_chase/scripts/home.js
+++ soccer_chase/scripts/home.js
@@ -1,11 +1,25 @@
-window.onload = function() {
-    const currentPage = window.location.pathname.split('/').pop();
-    const navItems = document.querySelectorAll('.nav-item');
-    navItems.forEach(item => {
-      if (item.href.includes(currentPage)) {
-        item.classList.add('active');
-      }
-    });
-  };
+window.onload = function () {
+  const currentPage = window.location.pathname.split('/').pop();
+  const navItems = document.querySelectorAll('.nav-item');
+  navItems.forEach(item => {
+    if (item.href.includes(currentPage)) {
+      item.classList.add('active');
+    }
+  });
+};
 
-  
+document.addEventListener("DOMContentLoaded", function () {
+  // Select the settings icon by its class
+  var settingsIcon = document.querySelector('.settings_icon');
+
+  // Check if the settingsIcon element exists
+  if (settingsIcon) {
+    // Add click event listener to the settings icon
+    settingsIcon.addEventListener('click', function () {
+      // Redirect to settings.html
+      window.location.href = 'settings.html';
+    });
+  } else {
+    console.error('Settings icon not found!');
+  }
+});
