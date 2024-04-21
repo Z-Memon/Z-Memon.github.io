6313fd856b2b79c472b27994f477cc18ab8f7462
Z-Memon <zehra.memon@georgebrown.ca>
2024-04-21 16:52:19 -0400

    score page update

--- soccer_chase/scripts/login.js
+++ soccer_chase/scripts/login.js
@@ -90,14 +90,3 @@ document.addEventListener("DOMContentLoaded", function() {
     resetBackground();
 });
 
-function createUserId() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random() * 16 | 0,
-            v = c === 'x' ? r : (r & 0x3 | 0x8);
-        return v.toString(16);
-    });
-}
-
-// After the userId is created
-let userId = createUserId();
-localStorage.setItem('userId', userId); // Store the userId in the localStorage
