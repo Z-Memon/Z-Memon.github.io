56578153f816e4ff0c835c24b0473090cb6f81b3
Z-Memon <zehra.memon@georgebrown.ca>
2024-04-25 19:42:54 -0400

    Update sign-out.js

--- soccer_chase/scripts/sign-out.js
+++ soccer_chase/scripts/sign-out.js
@@ -19,6 +19,17 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
+function clearAllCookies() {
+  var cookies = document.cookie.split(";");
+
+  for (var i = 0; i < cookies.length; i++) {
+      var cookie = cookies[i];
+      var eqPos = cookie.indexOf("=");
+      var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+      document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+  }
+}
+
 const logout = document.getElementById('logout');
 logout.addEventListener('click', function () {
     signOut(auth).then(() => {
