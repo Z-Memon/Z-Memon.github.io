c1c57da28796f78ec41fc9c0e98d25b65390b7e8
Z-Memon <zehra.memon@georgebrown.ca>
2024-04-29 00:56:09 -0400

    Update ranking.js

--- soccer_chase/scripts/ranking.js
+++ soccer_chase/scripts/ranking.js
@@ -58,7 +58,7 @@ window.onload = function() {
       rankItem.addEventListener('click', function() {
         localStorage.setItem('currentUserData', JSON.stringify(userData));
       // Navigate to the user stats page
-      window.location.href = 'Score.html'; // Replace with the actual path to your user stats page
+      window.location.href = 'score.html'; // Replace with the actual path to your user stats page
 
         document.querySelector('#email').textContent = userData.email; // Assuming the points earned are stored here
         document.querySelector('#points-earned').textContent = userData.pointsEarned; // Assuming the points earned are stored here
